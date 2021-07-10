5d153522065a86337f7c1ddff60dcabc097e3d28
Abdullah1426230 <85074687+Abdullah1426230@users.noreply.github.com>
2021-07-10 15:21:42 +0300

    Delete js.js

--- js.js
+++
@@ -1,4 +0,0 @@
-var color = document.getElementById("color").value;
-var con = document.getElementsById("con");
-con.style.backgroundColor = color + " !important";
-localStorage.setItem("color", color);
